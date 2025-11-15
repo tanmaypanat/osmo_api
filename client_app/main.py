c2ecfb63d6a964c70958fcb4486b0d213fb73cb7
tanmaypanat <panattanmay@gmail.com>
2025-11-15 17:29:27 -0500

    add client loop

--- client_app/main.py
+++ client_app/main.py
@@ -1,5 +1,6 @@
 import requests
 
+
 url = "http://localhost:8080/formulas"
 
 
@@ -8,10 +9,53 @@ formula_1 = {
     "materials": [
         {"name": "Ethanol", "concentration": 11.23},
         {"name": "vanilla Oil", "concentration": 5.00},
-        {"name": "sea salt", "concentration": 13.7},
+        {"name": "sea salt", "concentration": 13.77},
     ],
 }
 
-response = requests.post(url, json=formula_1)
-print("\nStatus Code:", response.status_code)
-print("Response Body:", response.json())
+formula_2 = {
+    "name": "Mountain Mist",
+    "materials": [
+        {"name": "Water", "concentration": 20.00},
+        {"name": "Pine Extract", "concentration": 7.50},
+        {"name": "Lemon Zest", "concentration": 12.50},
+    ],
+}
+
+invalid_formula = {
+    "name": "Invalid Formula",
+    "materials": [
+        {"name": "Unknown Substance", "concentration": "high"},
+        {"name": "Mystery Ingredient", "concentration": -5},
+    ],
+}
+
+while True:
+    choice = (
+        input(
+            "\n1.Send formula 1\n2.Send formula 2 \n3.Send invalid formula\n4.Get all formulas\n\n"
+        )
+        .strip()
+        .lower()
+    )
+    if choice == "1":
+        selected_formula = formula_1
+        response = requests.post(url, json=selected_formula)
+        print("\nStatus Code:", response.status_code)
+        print("Response Body:", response.json())
+    elif choice == "2":
+        selected_formula = formula_2
+        response = requests.post(url, json=selected_formula)
+        print("\nStatus Code:", response.status_code)
+        print("Response Body:", response.json())
+    elif choice == "3":
+        selected_formula = invalid_formula
+        response = requests.post(url, json=selected_formula)
+        print("\nStatus Code:", response.status_code)
+        print("Response Body:", response.json())
+    elif choice == "4":
+        response = requests.get(url)
+        print("\nStatus Code:", response.status_code)
+        print("Response Body:", response.json())
+    else:
+        print("Invalid choice. Please enter 1 or 2.")
